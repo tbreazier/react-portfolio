fc0a319505acdf40bdb0000efe4d629ac09612de
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-10-05 21:52:44 -0500

    started portfolio

---
+++ src/components/Nav/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Nav() {
+
+  return (
+      <header>
+          <nav>
+              <ul className="flex-row">
+                  <li className="mx-2">
+                      <a href="#about">
+                          About
+                    </a>
+                  </li>
+                  <li className="mx-2">
+                      <a href="#projects">
+                          Projects
+                    </a>
+                  </li>
+                  <li className="mx-2">
+                      <a href="#resume">
+                          Resume
+                    </a>
+                  </li>
+                  <li className="mx-2">
+                      <a href="#contact">
+                          Contact Me
+                    </a>
+                  </li>
+              </ul>
+          </nav>
+      </header>
+  );
+}
+
+export default Nav;
