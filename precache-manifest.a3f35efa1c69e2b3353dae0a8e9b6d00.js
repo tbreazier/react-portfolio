9a7bca077e197037476da8ea38cefffe7fb50892
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-10-06 22:32:08 -0500

    Updates

---
+++ precache-manifest.a3f35efa1c69e2b3353dae0a8e9b6d00.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bbbb82185e0de729ac14fd0d26eb9a5",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b693597b2383f66d3a69",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8a72d6172309a9d64c69",
+    "url": "/react-portfolio/static/css/main.e41b8d89.chunk.css"
+  },
+  {
+    "revision": "b693597b2383f66d3a69",
+    "url": "/react-portfolio/static/js/2.a3cd3110.chunk.js"
+  },
+  {
+    "revision": "1d9124e44d5667f964570d4ec46a0832",
+    "url": "/react-portfolio/static/js/2.a3cd3110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a72d6172309a9d64c69",
+    "url": "/react-portfolio/static/js/main.ec339aa9.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "425c31be2c27b4b5063f4987393e46f6",
+    "url": "/react-portfolio/static/media/bored.425c31be.png"
+  },
+  {
+    "revision": "15df3a30fea0cfe664ee37d4c92ac060",
+    "url": "/react-portfolio/static/media/cover4.15df3a30.jpg"
+  },
+  {
+    "revision": "11d046fa2159f691a9b5708aad8ecc5f",
+    "url": "/react-portfolio/static/media/note.11d046fa.png"
+  },
+  {
+    "revision": "8eae48006c69fc5cdca988cf2c3ddc44",
+    "url": "/react-portfolio/static/media/profile1.8eae4800.png"
+  },
+  {
+    "revision": "241d4153b3a4a9589ef1dbbdd33cac3a",
+    "url": "/react-portfolio/static/media/readme.241d4153.png"
+  },
+  {
+    "revision": "dfabc6c14e24cd24ac286b438b663ca6",
+    "url": "/react-portfolio/static/media/resume.dfabc6c1.pdf"
+  },
+  {
+    "revision": "f679c8006c26cc2126e84264c6d5b08a",
+    "url": "/react-portfolio/static/media/simplesocial.f679c800.jpg"
+  },
+  {
+    "revision": "24306ef973a7db702fae59b9d6124ced",
+    "url": "/react-portfolio/static/media/weather.24306ef9.png"
+  },
+  {
+    "revision": "41ed49c52cf5390a1a02cf96af961773",
+    "url": "/react-portfolio/static/media/workday.41ed49c5.png"
+  }
+]);
